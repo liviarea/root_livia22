a182420a1345c3835c6973f010abf9cff05457b0
livia <6312322@edu.vaniercollege.qc.ca>
2025-08-27 15:53:18 -0400

    ttt

--- public_html/web3/JS/app.js
+++ public_html/web3/JS/app.js
@@ -3,9 +3,9 @@ let cases = document.querySelectorAll(".case");
 let replayBtn = document.querySelector("#replay");
 let panneauMessage = document.querySelector("#message");
 
-let joueurX = true 
-let gagnant = ''
-const patron = [
+let joueurX = true; 
+let gagnant = '';
+const patrons = [
     [0, 1, 2],
     [0, 3, 6],
     [0, 4, 8],
@@ -17,24 +17,24 @@ const patron = [
 ]
 
 for (let boite of cases) {
-    boite.active = true
+    boite.active = true;
    boite.addEventListener( "click", function() {
    if (boite.active) {
         if (joueurX) {
-    boite.innerText = "X";
-    joueurX = false;
+           boite.innerText = "X";
+            joueurX = false;
+        }
+            else {
+            boite.innerText = "O";
+            joueurX = true;
+        }
    }
-    else {
-     boite.innerText = "O";
-     joueurX = true;
-   }
-   }
-   
+   boite.active = false;
+valide();
    });
 }
 
-boite.active = false;
-valide();
+
 
 replayBtn.addEventListener("click", function() {
         for (let boite of cases) {
@@ -45,12 +45,12 @@ replayBtn.addEventListener("click", function() {
 })
 
 const valide = function () {
-    for (let patron of patron) { 
+    for (let patron of patrons) { 
         let val1 = cases [patron[0]].innerText
         let val2 = cases [patron[1]].innerText
         let val3 = cases [patron[2]].innerText
 
-        if (val &&
+        if (val1 &&
             val1 === val2 &&
             val1 === val3) {
         console.log(`Le gagnant est ${val1}`);
